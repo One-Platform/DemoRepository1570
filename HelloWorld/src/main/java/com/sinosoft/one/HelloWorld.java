1ad3a45a7fcabbc89560a1c1fb1acc5b2dddd0d3
yangzongbin <shmilbin1570@126.com>
2013-12-06 11:06:08 +0800

    增加 helloworld

---
+++ HelloWorld/src/main/java/com/sinosoft/one/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.sinosoft.one;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bin
+ * Date: 13-12-6
+ * Time: 上午11:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("hello world git");
+    }
+}
