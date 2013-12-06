da815b75fead9410ce980e7b0ab3c9e6b1d54e8e
yangzongbin <shmilbin1570@126.com>
2013-12-06 15:10:37 +0800

    增加address,name属性
    增加增加address,name属性

--- HelloWorld/src/main/java/com/sinosoft/one/Company.java
+++ HelloWorld/src/main/java/com/sinosoft/one/Company.java
@@ -8,4 +8,23 @@ package com.sinosoft.one;
  * To change this template use File | Settings | File Templates.
  */
 public class Company {
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
