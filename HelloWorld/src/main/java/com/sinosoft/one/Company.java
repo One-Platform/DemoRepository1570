addbc4b3a167050692d9e9bad6e2f2a4f822751e
yangzongbin <shmilbin1570@126.com>
2013-12-06 14:55:29 +0800

    增加Company类

---
+++ HelloWorld/src/main/java/com/sinosoft/one/Company.java
@@ -0,0 +1,11 @@
+package com.sinosoft.one;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bin
+ * Date: 13-12-6
+ * Time: 下午2:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class Company {
+}
